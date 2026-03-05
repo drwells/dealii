4a5d5fd30277271271e204639a7e64535d1ccbde
David Wells <drwells@email.unc.edu>
2026-03-05 16:40:05 -0500

    start

---
+++ include/deal.II/base/std_cxx26/inplace_vector.h
@@ -0,0 +1,745 @@
+// ------------------------------------------------------------------------
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2026 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// Part of the source code is dual licensed under Apache-2.0 WITH
+// LLVM-exception OR LGPL-2.1-or-later. Detailed license information
+// governing the source code and code contributions can be found in
+// LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+//
+// ------------------------------------------------------------------------
+#ifndef dealii_cxx26_inplace_vector_h
+#define dealii_cxx26_inplace_vector_h
+
+#include <deal.II/base/config.h>
+
+#ifdef DEAL_II_HAVE_CXX26
+#  include <inplace_vector>
+#else
+#  include <algorithm>
+#  include <array>
+#  include <initializer_list>
+#  include <iterator>
+#  include <limits>
+#  include <type_traits>
+#  include <utility>
+#endif
+
+DEAL_II_NAMESPACE_OPEN
+
+namespace std_cxx26
+{
+#ifndef DEAL_II_HAVE_CXX26
+  /**
+   * C++17-implementation of a subset of std::inplace_vector.
+   *
+   * Differences:
+   *
+   * 1. The number of elements is limited to the maximum value of an
+   *    `unsigned short`, which is typically 65535.
+   * 2. Since placement new() is not constexpr prior to C++26, the majority of
+   *    the constructors and assignment operators are not constexpr.
+   * 3. The range constructor is not present sinces ranges were not available
+   *    prior to C++20.
+   *
+   * See https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2024/p2747r2.html
+   * for more information.
+   */
+  template <typename T, std::size_t N>
+  class inplace_vector
+  {
+  public:
+    /**
+     * Types.
+     */
+    /** @{ */
+    using value_type      = T;
+    using size_type       = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference       = value_type &;
+    using const_reference = const value_type &;
+    using pointer         = value_type *;
+    using const_pointer   = const value_type *;
+    using iterator        = pointer;
+    using const_iterator  = const_pointer;
+
+    using reverse_iterator       = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    /** @} */
+
+    /**
+     * Constructors and destructor.
+     */
+    /** @{ */
+    constexpr inplace_vector() noexcept
+      : n_elements(0)
+    {}
+
+    explicit inplace_vector(size_type n)
+      : n_elements(0)
+    {
+      internal_resize<true>(n);
+    }
+
+    inplace_vector(size_type n, const T &value)
+      : n_elements(0)
+    {
+      if (n > N)
+        throw std::bad_alloc();
+      internal_append(SingleInputIterator(&value, 0),
+                      SingleInputIterator(&value, n));
+    }
+
+    template <class InputIterator>
+    inplace_vector(InputIterator first, InputIterator last)
+      : n_elements(0)
+    {
+      // If we have two integers (e.g., inplace_vector(42, 42)) then we should
+      // convert to the correct types and call the other constructor
+      if constexpr (std::is_integral_v<InputIterator>)
+        *this =
+          inplace_vector(static_cast<size_type>(first), static_cast<T>(last));
+      else
+        internal_append<InputIterator, true, false>(first, last);
+    }
+
+    inplace_vector(const inplace_vector &other)
+      : n_elements(0)
+    {
+      internal_append(other.begin(), other.end());
+    }
+
+    inplace_vector(inplace_vector &&other) noexcept(
+      N == 0 || std::is_nothrow_move_constructible_v<T>)
+      : n_elements(0)
+    {
+      internal_append<iterator, false, true>(other.begin(), other.end());
+      other.clear();
+    }
+
+    constexpr inplace_vector(std::initializer_list<T> other)
+      : n_elements(0)
+    {
+      if (other.size() > N)
+        throw std::bad_alloc();
+      internal_append(other.begin(), other.end());
+    }
+
+#  ifdef DEAL_II_WITH_CXX20
+    constexpr
+#  endif
+      ~inplace_vector()
+    {
+      clear();
+    }
+    /** @} */
+
+    /**
+     * Assignment operators.
+     */
+    /** @{ */
+    inplace_vector &
+    operator=(const inplace_vector &other)
+    {
+      internal_assign(other.begin(), other.end());
+
+      return *this;
+    }
+
+    inplace_vector &
+    operator=(inplace_vector &&other) noexcept(
+      N == 0 || (std::is_nothrow_move_assignable_v<T> &&
+                 std::is_nothrow_move_constructible_v<T>))
+    {
+      internal_assign<iterator, false, true>(other.begin(), other.end());
+
+      return *this;
+    }
+
+    inplace_vector &
+    operator=(std::initializer_list<T> other)
+    {
+      if (other.size() > N)
+        throw std::bad_alloc();
+
+      internal_assign(other.begin(), other.end());
+
+      return *this;
+    }
+    /** @} */
+
+    /**
+     * Assignment functions.
+     */
+    /** @{ */
+    template <class InputIterator>
+    void
+    assign(InputIterator first, InputIterator last)
+    {
+      internal_assign(first, last);
+    }
+
+    void
+    assign(size_type n, const T &value)
+    {
+      if (n > N)
+        throw std::bad_alloc();
+
+      internal_assign(SingleInputIterator(&value, 0),
+                      SingleInputIterator(&value, n));
+    }
+
+    void
+    assign(std::initializer_list<T> other)
+    {
+      if (other.size() > N)
+        throw std::bad_alloc();
+
+      internal_assign(other.begin(), other.end());
+    }
+    /** @} */
+
+    /**
+     * Iterators.
+     */
+    /** @{ */
+    constexpr iterator
+    begin() noexcept
+    {
+      return std::addressof(elements[0]);
+    }
+
+    constexpr iterator
+    end() noexcept
+    {
+      return std::addressof(elements[0]) + size();
+    }
+
+    constexpr const_iterator
+    begin() const noexcept
+    {
+      return std::addressof(elements[0]);
+    }
+
+    constexpr const_iterator
+    end() const noexcept
+    {
+      return std::addressof(elements[0]) + size();
+    }
+
+    constexpr reverse_iterator
+    rbegin() noexcept
+    {
+      return reverse_iterator(end());
+    }
+
+    constexpr reverse_iterator
+    rend() noexcept
+    {
+      return reverse_iterator(begin());
+    }
+
+    constexpr const_reverse_iterator
+    rbegin() const noexcept
+    {
+      return reverse_iterator(end());
+    }
+
+    constexpr const_reverse_iterator
+    rend() const noexcept
+    {
+      return reverse_iterator(begin());
+    }
+
+    constexpr const_iterator
+    cbegin() const noexcept
+    {
+      return std::addressof(elements[0]);
+    }
+
+    constexpr const_iterator
+    cend() const noexcept
+    {
+      return std::addressof(elements[0]) + size();
+    }
+
+    constexpr const_reverse_iterator
+    crbegin() const noexcept
+    {
+      return reverse_iterator(cend());
+    }
+
+    constexpr const_reverse_iterator
+    crend() const noexcept
+    {
+      return reverse_iterator(cbegin());
+    }
+    /** @} */
+
+    /**
+     * Capacity.
+     */
+    /** @{ */
+    constexpr bool
+    empty() const noexcept
+    {
+      return size() == 0;
+    }
+
+    constexpr size_type
+    size() const noexcept
+    {
+      return n_elements;
+    }
+
+    static constexpr size_type
+    max_size() noexcept
+    {
+      return N;
+    }
+
+    static constexpr size_type
+    capacity() noexcept
+    {
+      return N;
+    }
+
+    void
+    resize(size_type n)
+    {
+      internal_resize(n);
+    }
+
+    void
+    resize(size_type n, const T &value)
+    {
+      internal_resize(n, value);
+    }
+
+    static constexpr void
+    reserve(size_type n)
+    {
+      if (n > N)
+        throw std::bad_alloc();
+    }
+
+    static constexpr void
+    shrink_to_fit() noexcept
+    {}
+    /** @} */
+
+    /**
+     * Element access.
+     */
+    /** @{ */
+    reference
+    operator[](size_type n)
+    {
+      // AssertIndexRange(n, size());
+      return elements[n];
+    }
+
+    const_reference
+    operator[](size_type n) const
+    {
+      // AssertIndexRange(n, size());
+      return elements[n];
+    }
+
+    reference
+    at(size_type n)
+    {
+      if (!(n < size()))
+        throw std::out_of_range();
+      return elements[n];
+    }
+
+    const_reference
+    at(size_type n) const
+    {
+      if (!(n < size()))
+        throw std::out_of_range();
+      return elements[n];
+    }
+
+    reference
+    front()
+    {
+      // Assert(!empty(), ExcEmptyObject());
+      return elements[0];
+    }
+
+    const_reference
+    front() const
+    {
+      // Assert(!empty(), ExcEmptyObject());
+      return elements[0];
+    }
+
+    reference
+    back()
+    {
+      // Assert(!empty(), ExcEmptyObject());
+      return elements[size() - 1];
+    }
+
+    const_reference
+    back() const
+    {
+      // Assert(!empty(), ExcEmptyObject());
+      return elements[size() - 1];
+    }
+    /** @} */
+
+    /**
+     * Data access.
+     */
+    /** @{ */
+    T *
+    data() noexcept
+    {
+      return begin();
+    }
+
+    const T *
+    data() const noexcept
+    {
+      return cbegin();
+    }
+    /** @} */
+
+    /**
+     * Modifiers.
+     */
+    /** @{ */
+    template <class... Args>
+    reference
+    emplace_back(Args &&...args)
+    {
+      internal_resize<true>(size() + 1, std::forward<Args>(args)...);
+    }
+
+    reference
+    push_back(const T &value)
+    {
+      internal_resize<true>(size() + 1, value);
+    }
+
+    reference
+    push_back(T &&value)
+    {
+      internal_resize<true>(size() + 1, std::forward(value));
+    }
+
+    void
+    pop_back()
+    {
+      // Assert(!empty(), ExcEmptyObject());
+      internal_resize(size() - 1);
+    }
+
+    template <class... Args>
+    pointer
+    try_emplace_back(Args &&...args)
+    {
+      if (size() == N)
+        return nullptr;
+      internal_resize(size() + 1, std::forward<Args>(args)...);
+      return std::addressof(back());
+    }
+
+    pointer
+    try_push_back(const T &value)
+    {
+      if (size() == N)
+        return nullptr;
+      internal_resize<true>(size() + 1, value);
+      return std::addressof(back());
+    }
+
+    pointer
+    try_push_back(T &&value)
+    {
+      if (size() == N)
+        return nullptr;
+      internal_resize<true>(size() + 1, std::forward(value));
+      return std::addressof(back());
+    }
+
+    template <class... Args>
+    reference
+    unchecked_emplace_back(Args &&...args)
+    {
+      // Assert(size() < capacity(), ExcCapacityExceeded());
+      internal_resize(size() + 1, std::forward<Args>(args)...);
+      return back();
+    }
+
+    reference
+    unchecked_push_back(const T &value)
+    {
+      // Assert(size() < capacity(), ExcCapacityExceeded());
+      internal_resize(size() + 1, value);
+      return back();
+    }
+
+    reference
+    unchecked_push_back(T &&value)
+    {
+      // Assert(size() < capacity(), ExcCapacityExceeded());
+      internal_resize(size() + 1, std::forward(value));
+      return back();
+    }
+
+    template <class... Args>
+    iterator
+    emplace(const_iterator position, Args &&...args)
+    {
+      const auto index = position - cbegin();
+      // Since args may reference *this we have to construct the new object
+      // first: do that in the buffer and then rotate so it is in the correct
+      // place
+      internal_resize<true>(size() + 1, std::forward<Args>(args)...);
+
+      std::rotate(begin() + index, end() - 1, end());
+
+      return begin() + index;
+    }
+
+    iterator
+    insert(const_iterator position, const T &value)
+    {
+      return emplace(position, value);
+    }
+
+    iterator
+    insert(const_iterator position, T &&value)
+    {
+      return emplace(position, std::move(value));
+    }
+
+    iterator
+    insert(const_iterator position, size_type n, const T &value)
+    {
+      return insert(position,
+                    SingleInputIterator(&value, 0),
+                    SingleInputIterator(&value, n));
+    }
+
+    template <typename InputIterator>
+    iterator
+    insert(const_iterator position, InputIterator first, InputIterator last)
+    {
+      const auto index = position - cbegin();
+      const auto n_new_elements =
+        internal_append<InputIterator, true>(first, last);
+      std::rotate(position, position + n_new_elements, end());
+      return begin() + index;
+    }
+
+    iterator
+    insert(const_iterator position, std::initializer_list<T> other)
+    {
+      return insert(position, other.begin(), other.end());
+    }
+
+    iterator
+    erase(const_iterator position)
+    {
+      const auto index = position - cbegin();
+      std::rotate(begin() + index, begin() + index + 1, end());
+      pop_back();
+
+      return begin() + index;
+    }
+
+    iterator
+    erase(const_iterator first, const_iterator last)
+    {
+      const auto index = first - begin();
+      std::rotate(first, last, end());
+      internal_resize(size() - (last - first));
+
+      return begin() + index;
+    }
+
+    void constexpr clear() noexcept
+    {
+      internal_resize(0);
+    }
+
+    /** @} */
+  private:
+    /**
+     * Wrapper class for an iterator-like interface to a single value.
+     *
+     * Most constructors and assign() overloads work with ranges of iterators.
+     * SingleInputIterator allows for calling those functions (or common
+     * utilities) from the functions which takes counts and values as well.
+     */
+    struct SingleInputIterator
+    {
+      using value_type        = const T;
+      using difference_type   = std::ptrdiff_t;
+      using reference         = const T &;
+      using pointer           = const T *;
+      using iterator_category = std::input_iterator_tag;
+
+      SingleInputIterator(const T *value, size_type index)
+        : ptr(value)
+        , index(index)
+      {}
+
+      reference
+      operator*() const
+      {
+        return *ptr;
+      }
+
+      bool
+      operator==(const SingleInputIterator &other) const
+      {
+        return ptr == other.ptr && index == other.index;
+      }
+
+      bool
+      operator!=(const SingleInputIterator &other) const
+      {
+        return !(*this == other);
+      }
+
+      SingleInputIterator &
+      operator++()
+      {
+        ++index;
+        return *this;
+      }
+
+      const T *ptr;
+
+      std::size_t index;
+    };
+
+    /**
+     * Common function for resizing the vector.
+     */
+    template <bool check = false, typename... Args>
+    constexpr void
+    internal_resize(const size_type n, Args &&...args) noexcept(!check)
+    {
+      static_assert(std::is_constructible_v<T, Args...>);
+      // We should always check the size unless we are clearing storage
+      // Assert(check || n == 0, ExcInternalError());
+      if constexpr (check)
+        if (n > N)
+          throw std::bad_alloc();
+
+      // Here and elsewhere we avoid std::uninitialized_copy() etc. so that we
+      // maintain the invariant that n_elements is always correct
+      if (n < size())
+        for (size_type i = size(); i > n; --i)
+          {
+            (end() - 1)->~T();
+            --n_elements;
+          }
+      else
+        for (size_type i = size(); i < n; ++i)
+          {
+            new (end()) T(args...);
+            ++n_elements;
+          }
+    }
+
+    /**
+     * Common function for assigning values to the vector from a range of input
+     * iterators, first by copying and then by placement new.
+     */
+    template <typename InputIterator, bool check = false, bool move = false>
+    void
+    internal_assign(InputIterator first, InputIterator last)
+    {
+      static_assert(std::is_convertible_v<decltype(*first), T>);
+      size_type i = 0;
+      while (i < size() && first != last)
+        {
+          if constexpr (move)
+            elements[i] = std::move(*first);
+          else
+            elements[i] = *first;
+          ++i;
+          ++first;
+        }
+      if (first == last)
+        resize(i);
+      else
+        internal_append<InputIterator, check, move>(first, last);
+    }
+
+    /**
+     * Common function for creating or moving new values at the end of the
+     * vector.
+     */
+    template <typename InputIterator, bool check = false, bool move = false>
+    constexpr size_type
+    internal_append(InputIterator first, InputIterator last)
+    {
+      static_assert(std::is_convertible_v<decltype(*first), T>);
+
+      // TODO: for constexpr-ification with C++17 we need to find a way to copy
+      // trivial values over without using new() (since that is not constexpr
+      // until C++26)
+
+      size_type count = 0;
+      while (first != last)
+        {
+          if constexpr (check)
+            if (size() == N)
+              throw std::bad_alloc();
+          if constexpr (move)
+            new (end()) T(std::move(*first));
+          else
+            new (end()) T(*first);
+          ++n_elements;
+          ++first;
+          ++count;
+        }
+      return count;
+    }
+
+    /**
+     * Prevent initialization of unused elements by placing the array in a
+     * union.
+     *
+     * @note we use std::array to utilize its specialization for N = 0.
+     */
+    union
+    {
+      std::array<T, N> elements;
+    };
+
+    static constexpr bool use_smaller_type =
+      N <= std::numeric_limits<unsigned char>::max();
+    using buffer_size_type =
+      std::conditional_t<use_smaller_type, unsigned char, unsigned short>;
+    static_assert(
+      N <= std::numeric_limits<unsigned short>::max(),
+      "This class only supports objects of size <= the maximum size of an "
+      "unsigned short (typically 65535).");
+
+    /**
+     * Present number of elements in the buffer.
+     */
+    buffer_size_type n_elements;
+  };
+
+#else
+  using std::inplace_vector;
+#endif
+} // namespace std_cxx26
+
+DEAL_II_NAMESPACE_CLOSE
+
+#endif
---
+++ tests/base/inplace_vector_01.cc
@@ -0,0 +1,218 @@
+// -----------------------------------------------------------------------------
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+// Copyright (C) 2026 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// Detailed license information governing the source code and contributions
+// can be found in LICENSE.md and CONTRIBUTING.md at the top level directory.
+//
+// -----------------------------------------------------------------------------
+
+
+// Test std_cxx26::inplace_vector's constructors.
+
+
+#include <deal.II/base/std_cxx26/inplace_vector.h>
+
+#include <numeric>
+#include <vector>
+
+#include "../tests.h"
+
+// Struct for recording which constructor is called and counting total ctor/dtor
+// calls
+struct A
+{
+  static int n_ctors;
+  static int n_dtors;
+
+  A()
+  {
+    ++n_ctors;
+    deallog << "A::A()" << std::endl;
+  }
+
+  A(A &&)
+  {
+    ++n_ctors;
+    deallog << "A::A(A&&)" << std::endl;
+  }
+
+  A(const A &)
+  {
+    ++n_ctors;
+    deallog << "A::A(const A&)" << std::endl;
+  }
+
+  ~A()
+  {
+    ++n_dtors;
+    deallog << "A::~A()" << std::endl;
+  }
+};
+
+int A::n_ctors = 0;
+int A::n_dtors = 0;
+
+template <typename T>
+void
+print_counts()
+{
+  if constexpr (std::is_same_v<T, A>)
+    deallog << "ctors = " << A::n_ctors << " dtors = " << A::n_dtors
+            << std::endl;
+}
+
+template <typename T>
+void
+print(const T &t)
+{
+  deallog << t;
+}
+
+template <typename T>
+void
+print(const std::vector<T> &vec)
+{
+  deallog << "{";
+  if (vec.size() > 0)
+    {
+      print(vec[0]);
+      for (std::size_t i = 1; i < vec.size(); ++i)
+        {
+          deallog << ", ";
+          print(vec[i]);
+        }
+    }
+  deallog << "}";
+}
+
+template <typename T, std::size_t N>
+void
+print(const std_cxx26::inplace_vector<T, N> &vec)
+{
+  if constexpr (!std::is_same_v<T, A>)
+    {
+      if (vec.size() > 0)
+        {
+          print(vec[0]);
+          for (std::size_t i = 1; i < vec.size(); ++i)
+            {
+              deallog << ", ";
+              print(vec[i]);
+            }
+        }
+    }
+  else
+    deallog << "size : " << vec.size();
+}
+
+template <typename T>
+void
+test_ctors()
+{
+  constexpr bool is_a = std::is_same_v<T, A>;
+
+  deallog << "default ctor" << std::endl;
+  {
+    std_cxx26::inplace_vector<T, 16> vec;
+    print(vec);
+    deallog << std::endl;
+  }
+  print_counts<T>();
+  deallog << std::endl;
+
+  deallog << "count default ctor" << std::endl;
+  {
+    std_cxx26::inplace_vector<T, 16> vec(2);
+    print(vec);
+    deallog << std::endl;
+  }
+  print_counts<T>();
+  deallog << std::endl;
+
+  deallog << "count copy ctor" << std::endl;
+  {
+    T a{};
+    if constexpr (std::is_same_v<T, int>)
+      a = 42;
+    if constexpr (std::is_same_v<T, std::vector<int>>)
+      a = {3, 5};
+    std_cxx26::inplace_vector<T, 16> vec(2, a);
+    print(vec);
+    deallog << std::endl;
+  }
+  print_counts<T>();
+  deallog << std::endl;
+
+  deallog << "iterator range, copy, and move ctor" << std::endl;
+  {
+    std::array<T, 2> as{};
+    if constexpr (std::is_same_v<T, int>)
+      std::iota(as.begin(), as.end(), -11);
+    if constexpr (std::is_same_v<T, std::vector<int>>)
+      {
+        as[0] = {1, 1, 2};
+        as[1] = {3, 5};
+      }
+    std_cxx26::inplace_vector<T, 16> vec1(as.begin(), as.end());
+    print(vec1);
+    deallog << std::endl;
+    deallog << "copy ctor" << std::endl;
+    std_cxx26::inplace_vector<T, 16> vec2(vec1);
+    print(vec2);
+    deallog << std::endl;
+    deallog << "and move ctor" << std::endl;
+    std_cxx26::inplace_vector<T, 16> vec3(std::move(vec1));
+    deallog << "moved-from:" << std::endl;
+    print(vec1);
+    deallog << std::endl;
+    deallog << "moved-to:" << std::endl;
+    print(vec3);
+    deallog << std::endl;
+  }
+  print_counts<T>();
+
+  deallog << "initializer_list ctor" << std::endl;
+  {
+    std::array<T, 2> as{};
+    if constexpr (std::is_same_v<T, int>)
+      std::iota(as.begin(), as.end(), -11);
+    if constexpr (std::is_same_v<T, std::vector<int>>)
+      {
+        as[0] = {1, 1, 2};
+        as[1] = {3, 5};
+      }
+    std_cxx26::inplace_vector<T, 16> vec(
+      std::initializer_list<T>{as[0], as[1]});
+    print(vec);
+    deallog << std::endl;
+  }
+  print_counts<T>();
+}
+
+int
+main()
+{
+  initlog();
+
+  deallog.push("ctors");
+  deallog << std::endl;
+  deallog.push("A");
+  test_ctors<A>();
+  deallog.pop();
+
+  deallog << std::endl;
+  deallog.push("int");
+  test_ctors<int>();
+  deallog.pop();
+
+  deallog << std::endl;
+  deallog.push("vector<int>");
+  test_ctors<std::vector<int>>();
+  deallog.pop();
+
+  deallog.pop();
+}
